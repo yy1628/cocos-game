c309ef7feddfeec4412b099a56c00e18cb34c613
yy <18502795380@163.com>
2024-11-20 18:03:51 +0800

    zstb2

---
+++ zstb2/cocos-js/spine.js-6770127a.js
@@ -0,0 +1,10 @@
+System.register([], (function (exports, module) {
+	'use strict';
+	return {
+		execute: (function () {
+
+			var spine_js = exports('default', 'assets/spine.js.mem-dc937d48.bin'); /* asset-hash:74cbe1d2 */
+
+		})
+	};
+}));
